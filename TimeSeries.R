5b1e734d7867dd548bd0a346710a0c6653bc4c33
Mario Quijada <141502739+Marioquijada28@users.noreply.github.com>
2023-11-24 21:50:59 +0000

    Update TimeSeries.R

--- TimeSeries.R
+++ TimeSeries.R
@@ -1,3 +1,4 @@
+#Time Series
 library(zoo)
 library(dplyr)
 library(ggplot2)
@@ -22,20 +23,20 @@ DENVTS <- summarise(DENVTS, CASES = n())
 tseries=ts(DENVTS$CASES, start = c(2000), frequency = 1)
 start(tseries); end(tseries)
 class(tseries)
-plot(tseries, ylab = "Cases", main = "Dengue Cases 2000-2022", col='red', lwd=3)
+plot(tseries, ylab = "Cases", main = "Cases 2000-2022", col='red', lwd=3)
 
 #Another option to create plot with point.
 ggplot() +
   geom_line(data = DENVTS, mapping = aes(x=YEAR, y=CASES),, color='black')+
   geom_point(data = DENVTS, mapping = aes(x=YEAR, y=CASES), color="black")
-labs(x="Year",y="Cases", main = "Time series of DENV 2000-2022")
+labs(x="Year",y="Cases", main = "Time series 2000-2022")
 
 #Another option to create plot with point and all year.
 ggplot() +
   geom_line(data = DENVTS, mapping = aes(x = year, y = CASES), color = 'black') +
   geom_point(data = DENVTS, mapping = aes(x = year, y = CASES), color = "black") +
   scale_x_continuous(breaks = seq(2000, 2022, by = 1)) +
-  labs(title = "DENV 2000-2022", x = "Year", y = "Cases") +
+  labs(title = "2000-2022", x = "Year", y = "Cases") +
   theme(axis.title = element_text(size = 25, face = "bold")) +
   theme(axis.text.x = element_text(size = 25, angle = 90, vjust = 1.5, hjust = 1)) +
   theme(axis.text.y = element_text(size = 25, vjust = 1, hjust = 1)) +
@@ -47,7 +48,7 @@ ggplot(data = DENVTS, aes(x = year, y = CASES)) +
   geom_point(color = "black") +
   geom_text(aes(label = CASES), vjust = -0.5, hjust = 0.5, size = 10) +  # Add this line
   scale_x_continuous(breaks = seq(2000, 2022, by = 1)) +
-  labs(title = "DENV 2000-2022", x = "Year", y = "Cases") +
+  labs(title = "2000-2022", x = "Year", y = "Cases") +
   theme(axis.title = element_text(size = 25, face = "bold")) +
   theme(axis.text.x = element_text(size = 25, angle = 90, vjust = 1.5, hjust = 1)) +
   theme(axis.text.y = element_text(size = 25, vjust = 1, hjust = 1)) +
